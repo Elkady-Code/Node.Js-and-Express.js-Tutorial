7ada071a4354b00eba0eabfe834cb026aa4faa7a
unknown <ibrahimallie905@gmail.com>
2024-07-29 19:53:39 +0300

    Middleware finished
    Fully understood logic of how middleware can be used

---
+++ 2-express-tutorial/final/9-middleware-use.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const logger = require("../logger");
+const authorize = require("../authorize");
+
+// req -> middleware -> res
+
+// 1. Use vs route
+// 2. Options - our own / express / third party
+
+// app.use([logger, authorize]);
+// app.use(express.static('./public'))
+
+app.use(morgan("tiny"));
+
+app.get("/", (req, res) => {
+  res.send("Home");
+});
+app.get("/about", (req, res) => {
+  res.send("About");
+});
+app.get("/api/products", (req, res) => {
+  res.send("Products");
+});
+app.get("/api/items", [logger, authorize], (req, res) => {
+  console.log(req.user);
+  res.send("Items");
+});
+
+app.all("*", logger, (req, res) => {
+  res.status(404).send("Sorry page is not found");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`The server is up and running on port ${port}`);
+});
